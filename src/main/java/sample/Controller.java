b0c7c428ecaf3f650b494152fa4b36ff966d9f77
Patryk <patrykdziedzioch@gmail.com>
2019-06-14 21:42:54 +0200

    Added BarChart

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,11 +1,9 @@
 package sample;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.chart.PieChart;
+import javafx.scene.Node;
+import javafx.scene.chart.*;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import pl.zankowski.iextrading4j.api.stocks.Quote;
 import pl.zankowski.iextrading4j.client.IEXCloudClient;
 import pl.zankowski.iextrading4j.client.IEXCloudTokenBuilder;
@@ -19,6 +17,12 @@ public class Controller {
     Label label;
     @FXML
     PieChart pieChart;
+    @FXML
+    NumberAxis xAxis;
+    @FXML
+    CategoryAxis yAxis;
+    @FXML
+    BarChart<String, Number> barChart;
 
     public void initialize() {
 
@@ -27,19 +31,40 @@ public class Controller {
                         .withPublishableToken("Tpk_18dfe6cebb4f41ffb219b9680f9acaf2")
                         .withSecretToken("Tsk_3eedff6f5c284e1a8b9bc16c54dd1af3")
                         .build());
-        final Quote quote = iexTradingClient.executeRequest(new QuoteRequestBuilder()
+        final Quote cisco = iexTradingClient.executeRequest(new QuoteRequestBuilder()
+                .withSymbol("CSCO")
+                .build());
+        final Quote apple = iexTradingClient.executeRequest(new QuoteRequestBuilder()
                 .withSymbol("AAPL")
                 .build());
-        System.out.println(quote.getWeek52High());
-        label.setText(String.valueOf(quote.getWeek52High()));
+        final Quote ibm = iexTradingClient.executeRequest(new QuoteRequestBuilder()
+                .withSymbol("IBM")
+                .build());
+        final Quote tencent = iexTradingClient.executeRequest(new QuoteRequestBuilder()
+                .withSymbol("TME")
+                .build());
+        //System.out.println(quote.getChange());
+        //label.setText(String.valueOf(quote.getWeek52High()));
+        xAxis.setLabel("IEX by volume");
+        xAxis.setTickLabelRotation(90);
+        yAxis.setLabel("Company");
+        barChart.setLegendVisible(false);
 
-        ObservableList<PieChart.Data> pieChartData =
-                FXCollections.observableArrayList(
-                        new PieChart.Data(quote.getLatestUpdate().toString(), quote.getLatestUpdate()),
-                        new PieChart.Data(quote.getOpenTime().toString(), quote.getOpenTime()),
-                        new PieChart.Data(quote.getCloseTime().toString(), quote.getCloseTime()));
-        pieChart.getData();
-        pieChart.setData(pieChartData);
+        XYChart.Series<String, Number> companyPrice = new XYChart.Series();
+        companyPrice.getData().add(new XYChart.Data( cisco.getSymbol(), cisco.getLatestPrice()));
+        companyPrice.getData().add(new XYChart.Data( apple.getSymbol(), apple.getLatestPrice()));
+        companyPrice.getData().add(new XYChart.Data( ibm.getSymbol(), ibm.getLatestPrice()));
+        companyPrice.getData().add(new XYChart.Data( tencent.getSymbol(), tencent.getLatestPrice()));
+        barChart.getData().add(companyPrice);
+
+        Node n = barChart.lookup(".data0.chart-bar");
+        n.setStyle("-fx-bar-fill: #ce712f");
+        n = barChart.lookup(".data1.chart-bar");
+        n.setStyle("-fx-bar-fill: #53a0e0");
+        n = barChart.lookup(".data2.chart-bar");
+        n.setStyle("-fx-bar-fill: #2d994f");
+        n = barChart.lookup(".data3.chart-bar");
+        n.setStyle("-fx-bar-fill: #dbd95e");
 
     }
 
